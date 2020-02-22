5b5d1d6e5937f55c7cb857e85e1fbd0cab1c97bd
Jordan <48123323+JordanSOA@users.noreply.github.com>
2020-02-22 22:17:16 +0100

    Acronym exo

---
+++ src/main/java/Acronym.java
@@ -0,0 +1,28 @@
+import com.sun.tools.javac.util.StringUtils;
+import jdk.internal.joptsimple.util.RegexMatcher;
+import jdk.nashorn.internal.runtime.regexp.RegExpMatcher;
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+public class Acronym {
+    private String phrase;
+
+    Acronym(String phrase) {
+        this.phrase = phrase;
+    }
+
+    String get() {
+        Pattern pat = Pattern.compile("'[a-zA-Z]|\\W|_");
+        String[] splitPhrase = pat.split(this.phrase, this.phrase.length());
+        String acronymRes = "";
+        for (int i = 0; i < splitPhrase.length; i++) {
+            if (!splitPhrase[i].isEmpty()){
+                char firstLetter = splitPhrase[i].charAt(0);
+                acronymRes += firstLetter;
+            }
+        }
+        return acronymRes.toUpperCase();
+    }
+}
